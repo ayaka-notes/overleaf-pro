840c4748f30601ba7c8486ba43cd338a62c46961
Musicminion <zhangziqian@sjtu.edu.cn>
2026-02-21 02:38:20 +0000

    revert #23 changes on services/filestore/app/js/FileConverter.js

--- services/filestore/app/js/FileConverter.js
+++ services/filestore/app/js/FileConverter.js
@@ -25,14 +25,12 @@ async function convert(sourcePath, requestedFormat) {
   const width = '600x'
   return await _convert(sourcePath, requestedFormat, [
     'convert',
-    // '-define',
-    // `pdf:fit-page=${width}`,
+    '-define',
+    `pdf:fit-page=${width}`,
     '-flatten',
     '-density',
     '300',
     `${sourcePath}[0]`,
-    `-resize`,
-    `1280x`,
   ])
 }
 
